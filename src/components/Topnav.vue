553548988a44583a4970e450a3d004f5eac166bd
Suno <dev.suno01@gmail.com>
2020-11-03 19:36:01 +0800

    更新全局样式，基本完成官网骨架

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -32,7 +32,10 @@
         background: pink;
         display: flex;
         padding: 16px;
-        position: relative;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
         z-index: 1;
         justify-content: center;
         align-items: center;
--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <Topnav/>
+    <div class="layout">
+        <Topnav class="nav"/>
         <div class="content">
             <aside v-if="menuVisible">
                 <h2>组件列表</h2>
@@ -38,9 +38,47 @@
     };
 </script>
 <style lang="scss" scoped>
+    .layout {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+
+        > .nav {
+            flex-shrink: 0;
+        }
+
+        > .content {
+            flex-grow: 1;
+            padding-top: 60px;
+            padding-left: 156px;
+            @media (max-width: 500px) {
+                padding-left: 0;
+            }
+        }
+    }
+
+    .content {
+        display: flex;
+
+        > aside {
+            flex-shrink: 0;
+        }
+
+        > main {
+            flex-grow: 1;
+            padding: 16px;
+            background: lightgreen;
+        }
+    }
+
     aside {
         background: lightblue;
         width: 150px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        padding-top: 70px;
+        height: 100%;
 
         > h2 {
             margin-bottom: 4px;
@@ -51,12 +89,9 @@
                 padding: 4px 0;
             }
         }
+    }
 
-        @media (max-width: 500px) {
-            position: fixed;
-            top: 0;
-            left: 0;
-            padding: 70px 0 0 16px;
-        }
+    main {
+        overflow: auto;
     }
 </style>
