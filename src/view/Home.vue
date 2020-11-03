0c2ec86d8efd40ca47bcb5b550ec274106c36eab
Suno <dev.suno01@gmail.com>
2020-11-03 14:10:04 +0800

    Home页面跳转 和 Doc页面 aside 搭建 HTML

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -6,8 +6,8 @@
             <h1>dingdang-ui</h1>
             <h2>一个厉害的 UI 框架</h2>
             <p class="actions">
-                <a>GitHub</a>
-                <a>开始</a>
+                <a href="https://github.com">GitHub</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
