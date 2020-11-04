e39b72ed527992bafb670e1d04af59a39dfc92f9
Suno <dev.suno01@gmail.com>
2020-11-04 15:29:18 +0800

    Switch组件的值从内部维护，改成父子传值

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <Switch/>
+        <Switch :value="x" @input="x = $event"/>
     </div>
 </template>
 
 <script lang="ts">
     import Switch from '../lib/Switch.vue';
+    import {ref} from 'vue'
     export default {
-        components:{Switch}
+        components:{Switch},
+        setup(){
+            const x = ref(false)
+            return {x}
+        }
     };
 </script>
 
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,17 +1,17 @@
 <template>
-    <button @click="toggle" :class="{checked}"><span></span></button>
+    <button :class="{checked : value}" @click="toggle"><span></span></button>
 </template>
 
 <script lang="ts">
-    import { ref } from 'vue';
-
     export default {
-        setup() {
-            const checked = ref(false);
+        props: {
+            value: Boolean
+        },
+        setup(props, context) {
             const toggle = () => {
-                checked.value = !checked.value;
+                context.emit('input', !props.value);
             };
-            return { checked, toggle };
+            return { toggle };
         }
     };
 </script>
@@ -36,6 +36,7 @@
         width: $h2;
         background: white;
         border-radius: $h2 / 2;
+        transition: left 250ms;
     }
 
     button.checked {
@@ -44,5 +45,9 @@
 
     button.checked > span {
         left: calc(100% - #{$h2} - 2px);
+    }
+
+    button:focus {
+        outline: none;
     }
 </style>
