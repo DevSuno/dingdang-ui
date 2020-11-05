cca2663710485a169592a183a0d39c2d42c1098a
Suno <dev.suno01@gmail.com>
2020-11-05 17:47:35 +0800

    添加 示例2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -14,6 +14,18 @@
         <Button>默认 Button</Button>
         <Button size="small">小小 Button</Button>
     </div>
+    <hr>
+    <div>
+        <Button theme="link" size="big">大大大</Button>
+        <Button theme="link">普普通</Button>
+        <Button size="small" theme="link">小小小</Button>
+    </div>
+    <hr>
+    <div>
+        <Button size="big" theme="text">大大大</Button>
+        <Button theme="text">普普通</Button>
+        <Button size="small" theme="text">小小小</Button>
+    </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -91,18 +91,16 @@
             }
         }
 
-        &.dingdang-theme-button {
-            &.dingdang-size-big {
-                font-size: 24px;
-                height: 48px;
-                padding: 0 16px
-            }
+        &.dingdang-size-big {
+            font-size: 24px;
+            height: 48px;
+            padding: 0 16px
+        }
 
-            &.dingdang-size-small {
-                font-size: 12px;
-                height: 20px;
-                padding: 0 4px;
-            }
+        &.dingdang-size-small {
+            font-size: 12px;
+            height: 20px;
+            padding: 0 4px;
         }
     }
 </style>
