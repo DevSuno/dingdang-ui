080a7bfc6a36ed25a1bb48ae86cfad3f84676cbb
Suno <dev.suno01@gmail.com>
2020-11-05 16:36:27 +0800

    测试事件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,16 +4,28 @@
     </div>
     <h1>示例 1</h1>
     <div>
-        <Button>我是一个 Button</Button>
+        <Button @click="onClick"
+                @focus="onClick"
+                @mouseover="onClick"
+        >我是一个 Button
+        </Button>
     </div>
 </template>
 
 <script lang="ts">
     import Button from '../lib/Button.vue';
+
     export default {
-        components:{
+        components: {
             Button
+        },
+        setup() {
+            const onClick = () => {
+                console.log('hi');
+            };
+            return { onClick };
         }
+
     };
 </script>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,25 @@
 <template>
-    <button>
-        <slot/>
-    </button>
+    <div :size="size">
+        <button v-bind="rest">
+            <slot/>
+        </button>
+    </div>
+
 </template>
 
 <script lang="ts">
-
-    export default  {
-
-    }
+    export default {
+        inheritAttrs: false,
+        props: {
+        },
+        setup(props, context) {
+            const { size, ...rest } = context.attrs;
+            return { size, rest };
+        },
+    };
 </script>
-
 <style lang="scss" scoped>
-
+    div {
+        border: 1px solid red;
+    }
 </style>
