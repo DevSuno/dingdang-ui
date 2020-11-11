92d612c45824fedd611cfe6f8c73a3fd55819f4d
Suno <dev.suno01@gmail.com>
2020-11-11 15:17:39 +0800

    让 Dialog 组件支持 visible 属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,27 @@
 <template>
     <div>
         dialog组件测试
+        <div>
+            <Button @click="toggle">toggle</Button>
+        </div>
     </div>
-    <Dialog></Dialog>
+    <Dialog :visible="x"></Dialog>
 </template>
 
 <script lang="ts">
     import Dialog from '../lib/Dialog.vue';
+    import Button from '../lib/Button.vue';
+    import { ref } from 'vue';
+
     export default {
-        components: {Dialog}
+        components: { Dialog, Button },
+        setup() {
+            const x = ref(false);
+            const toggle = () => {
+                x.value = !x.value;
+            };
+            return { x, toggle };
+        }
     };
 </script>
 
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,24 +1,32 @@
 <template>
-    <div class="dingdang-dialog-overlay"></div>
-    <div class="dingdang-dialog-wrapper">
-        <div class="dingdang-dialog">
-            <header>标题<span class="dingdang-dialog-close"></span></header>
-            <main>
-                <p>第一行</p>
-                <p>第二行</p>
-            </main>
-            <footer>
-                <Button>OK</Button>
-                <Button>Cancel</Button>
-            </footer>
+    <template v-if="visible">
+        <div class="dingdang-dialog-overlay"></div>
+        <div class="dingdang-dialog-wrapper">
+            <div class="dingdang-dialog">
+                <header>标题<span class="dingdang-dialog-close"></span></header>
+                <main>
+                    <p>第一行</p>
+                    <p>第二行</p>
+                </main>
+                <footer>
+                    <Button>OK</Button>
+                    <Button>Cancel</Button>
+                </footer>
+            </div>
         </div>
-    </div>
+    </template>
 </template>
 
 <script lang="ts">
     import Button from './Button.vue';
 
     export default {
+        props: {
+            visible: {
+                type: Boolean,
+                default: false,
+            }
+        },
         components: { Button }
     };
 </script>
@@ -32,6 +40,7 @@
         box-shadow: 0 0 3px fade_out(black, 0.5);
         min-width: 15em;
         max-width: 90%;
+
         &-overlay {
             position: fixed;
             top: 0;
@@ -41,6 +50,7 @@
             background: fade_out(black, 0.5);
             z-index: 10;
         }
+
         &-wrapper {
             position: fixed;
             left: 50%;
@@ -48,7 +58,8 @@
             transform: translate(-50%, -50%);
             z-index: 11;
         }
-        >header {
+
+        > header {
             padding: 12px 16px;
             border-bottom: 1px solid $border-color;
             display: flex;
@@ -56,20 +67,24 @@
             justify-content: space-between;
             font-size: 20px;
         }
-        >main {
+
+        > main {
             padding: 12px 16px;
         }
-        >footer {
+
+        > footer {
             border-top: 1px solid $border-color;
             padding: 12px 16px;
             text-align: right;
         }
+
         &-close {
             position: relative;
             display: inline-block;
             width: 16px;
             height: 16px;
             cursor: pointer;
+
             &::before,
             &::after {
                 content: '';
@@ -80,9 +95,11 @@
                 top: 50%;
                 left: 50%;
             }
+
             &::before {
                 transform: translate(-50%, -50%) rotate(-45deg);
             }
+
             &::after {
                 transform: translate(-50%, -50%) rotate(45deg);
             }
