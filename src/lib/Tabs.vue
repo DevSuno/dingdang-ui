5cfdb590188258eb6c27596b87e237b7e365306f
Suno <dev.suno01@gmail.com>
2020-11-12 20:09:50 +0800

    实现滚动的下划线

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="dingdang-tabs">
-        <div class="dingdang-tabs-nav">
+        <div class="dingdang-tabs-nav" ref="container">
             <div :class="{selected: t=== selected}"
                  :key="index"
+                 :ref="el => { if (el) navItems[index] = el }"
                  @click="select(t)"
                  class="dingdang-tabs-nav-item"
                  v-for="(t,index) in titles">
                 {{t}}
             </div>
-            <div class="dingdang-tabs-nav-indicator"></div>
+            <div class="dingdang-tabs-nav-indicator" ref="indicator"></div>
 
         </div>
         <div class="dingdang-tabs-content">
@@ -22,7 +23,7 @@
 
 <script lang="ts">
     import Tab from './Tab.vue';
-    import { computed } from 'vue';
+    import { computed, ref, onMounted, onUpdated } from 'vue';
 
     export default {
         props: {
@@ -31,6 +32,29 @@
             }
         },
         setup(props, context) {
+            const navItems = ref<HTMLDivElement[]>([]);
+            const indicator = ref<HTMLDivElement>(null);
+            const container = ref<HTMLDivElement>(null);
+            const x = () => {
+                const divs = navItems.value;
+                const result = divs.filter(div => div.classList.contains('selected'))[0];
+                console.log(result);
+                const {
+                    width
+                } = result.getBoundingClientRect();
+                indicator.value.style.width = width + 'px';
+                const {
+                    left: left1
+                } = container.value.getBoundingClientRect();
+                const {
+                    left: left2
+                } = result.getBoundingClientRect();
+                const left = left2 - left1;
+                indicator.value.style.left = left + 'px';
+            };
+            onMounted(x);
+            onUpdated(x);
+
             const defaults = context.slots.default();
             defaults.forEach((tag) => {
                 if (tag.type !== Tab) {
@@ -52,7 +76,10 @@
                 defaults,
                 titles,
                 current,
-                select
+                select,
+                navItems,
+                indicator,
+                container
             };
         }
     };
@@ -90,6 +117,7 @@
                 left: 0;
                 bottom: -1px;
                 width: 100px;
+                transition: all 250ms;
             }
         }
 
