c666e3a9230674a8c98bdfcd834145c2fcfa3c0f
Suno <dev.suno01@gmail.com>
2020-11-13 12:03:48 +0800

    优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -23,7 +23,7 @@
 
 <script lang="ts">
     import Tab from './Tab.vue';
-    import { computed, ref, onMounted, onUpdated } from 'vue';
+    import { computed, ref, onMounted, watchEffect } from 'vue';
 
     export default {
         props: {
@@ -35,16 +35,16 @@
             const selectedItem = ref<HTMLDivElement>(null);
             const indicator = ref<HTMLDivElement>(null);
             const container = ref<HTMLDivElement>(null);
-            const x = () => {
-                const { width } = selectedItem.value.getBoundingClientRect();
-                indicator.value.style.width = width + 'px';
-                const { left: left1 } = container.value.getBoundingClientRect();
-                const { left: left2 } = selectedItem.value.getBoundingClientRect();
-                const left = left2 - left1;
-                indicator.value.style.left = left + 'px';
-            };
-            onMounted(x);
-            onUpdated(x);
+            onMounted(() => {
+                watchEffect(() => {
+                    const { width } = selectedItem.value.getBoundingClientRect();
+                    indicator.value.style.width = width + 'px';
+                    const { left: left1 } = container.value.getBoundingClientRect();
+                    const { left: left2 } = selectedItem.value.getBoundingClientRect();
+                    const left = left2 - left1;
+                    indicator.value.style.left = left + 'px';
+                });
+            });
 
             const defaults = context.slots.default();
             defaults.forEach((tag) => {
@@ -70,7 +70,7 @@
                 select,
                 selectedItem,
                 indicator,
-                container
+                container,
             };
         }
     };
