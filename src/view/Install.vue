15ce59d21857294200b3466de6fdfb4cedb63aed
Suno <dev.suno01@gmail.com>
2020-11-15 23:23:37 +0800

    HTML 内容 和 引入 github-markdown-css 样式

--- src/view/Install.vue
+++ src/view/Install.vue
@@ -1,14 +1,19 @@
 <template>
-    <div>
-        安装
-    </div>
+    <article class="markdown-body">
+        <h1>安装</h1>
+        <p>打开终端运行下列命令：</p>
+        <pre><code>npm install dingdang-ui</code></pre>
+        <p>或</p>
+        <pre><code>yarn add dingdang-ui</code></pre>
+        <p>
+            下一节：<a href="#/doc/get-started">开始使用</a>
+        </p>
+    </article>
 </template>
 
 <script lang="ts">
 
-    export default {
-
-    }
+    export default {};
 </script>
 
 <style lang="scss" scoped>
--- src/view/Intro.vue
+++ src/view/Intro.vue
@@ -1,14 +1,17 @@
 <template>
-    <div>
-        介绍
-    </div>
+    <article class="markdown-body">
+        <h1>介绍</h1>
+        <p>King UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+        <p>这款组件库是为了总结自己的技术经验而写，全程亲手编写，尽量不采用第三方库，包括你现在看到的这个官网也几乎都是我自己写的。</p>
+        <p>所以如果强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。源代码放在了
+            github.com/devsuno/dingdang-ui，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见 README.md。</p>
+        下一节：<a href="#/doc/install">安装</a>
+    </article>
 </template>
 
 <script lang="ts">
 
-    export default {
-
-    }
+    export default {};
 </script>
 
 <style lang="scss" scoped>
