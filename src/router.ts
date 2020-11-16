68ffd090f4fd491ce463adfc08416d793bece0d0
Suno <dev.suno01@gmail.com>
2020-11-17 00:10:09 +0800

    把要用的 markdown 抽离出来做成组件

--- src/router.ts
+++ src/router.ts
@@ -6,27 +6,29 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DocDemo from './components/DocDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
-import GetStarted from './view/GetStarted.vue';
-import Install from './view/Install.vue';
-import Intro from './view/Intro.vue';
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory();
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
+
 export const router = createRouter({
     history: history,
+
     routes: [
         { path: '/', component: Home },
         {
             path: '/doc',
             component: Doc,
             children: [
-                { path: "", component: DocDemo },
-                { path: "get-started", component: GetStarted },
-                { path: "install", component: Install },
-                { path: "intro", component: Intro },
-                { path: "switch", component: SwitchDemo },
-                { path: "button", component: ButtonDemo },
-                { path: "dialog", component: DialogDemo },
-                { path: "tabs", component: TabsDemo },
+                { path: '', component: DocDemo },
+                { path: 'get-started', component: md('get-started') },
+                { path: 'install', component: md('install')},
+                { path: 'intro', component: md('intro')},
+                { path: 'switch', component: SwitchDemo },
+                { path: 'button', component: ButtonDemo },
+                { path: 'dialog', component: DialogDemo },
+                { path: 'tabs', component: TabsDemo },
             ],
         },
     ],
