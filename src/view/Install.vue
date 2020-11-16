54f60508b03625cf4d4554e211f7c5fc31b37650
Suno <dev.suno01@gmail.com>
2020-11-16 23:58:01 +0800

    markdown 文档完成

--- src/view/Install.vue
+++ src/view/Install.vue
@@ -1,19 +1,15 @@
 <template>
-    <article class="markdown-body">
-        <h1>安装</h1>
-        <p>打开终端运行下列命令：</p>
-        <pre><code>npm install dingdang-ui</code></pre>
-        <p>或</p>
-        <pre><code>yarn add dingdang-ui</code></pre>
-        <p>
-            下一节：<a href="#/doc/get-started">开始使用</a>
-        </p>
+    <article class="markdown-body" v-html="md">
     </article>
 </template>
 
-<script lang="ts">
-
-    export default {};
+<script>
+    import md from '../markdown/install.md'
+    export default {
+        data(){
+            return {md}
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
