f8c55b019439a87e265a5c04ab2186ecc2529893
Suno <dev.suno01@gmail.com>
2020-11-17 16:18:03 +0800

    再次封装 Demo 展示

---
+++ src/components/Demo.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="demo">
+        <h2>常规用法</h2>
+        <div class="demo-component">
+            <component :is="components"></component>
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+                  <pre class="language-html"
+                       v-html="Prism.highlight( components.__sourceCode,Prism.languages.html, 'html')"/>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+    import Button from '../lib/Button.vue';
+    import 'prismjs';
+    import 'prismjs/themes/prism.css'
+
+    const Prism = (window as any).Prism;
+    export default {
+        props: {
+            components: Object,
+        },
+        setup() {
+            return { Prism, Button };
+        }
+    };
+</script>
+
+<style lang="scss">
+    $border-color: #d9d9d9;
+    .demo {
+        border: 1px solid $border-color;
+        margin: 16px 0 32px;
+
+        > h2 {
+            font-size: 20px;
+            padding: 8px 16px;
+            border-bottom: 1px solid $border-color;
+        }
+
+        &-component {
+            padding: 16px;
+        }
+
+        &-actions {
+            padding: 8px 16px;
+            border-top: 1px dashed $border-color;
+        }
+
+        &-code {
+            padding: 8px 16px;
+            border-top: 1px dashed $border-color;
+
+            > pre {
+                line-height: 1.1;
+                font-family: Consolas, 'Courier New', Courier, monospace;
+                margin: 0;
+            }
+        }
+    }
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,32 +1,8 @@
 <template>
     <div>
         <h1>Switch 组件示例 </h1>
-        <div class="demo">
-            <h2>常规用法</h2>
-            <div class="demo-component">
-                <component :is="Switch1Demo"></component>
-            </div>
-            <div class="demo-actions">
-                <Button>查看代码</Button>
-            </div>
-            <div class="demo-code">
-                  <pre class="language-html"
-                      v-html="Prism.highlight( Switch2Demo.__sourceCode,Prism.languages.html, 'html')"/>
-            </div>
-        </div>
-        <div class="demo">
-            <h2>支持 disabled </h2>
-            <div class="demo-component">
-                <component :is="Switch2Demo"></component>
-            </div>
-            <div class="demo-actions">
-                <Button>查看代码</Button>
-            </div>
-            <div class="demo-code">
-                <pre class="language-html"
-                     v-html="Prism.highlight( Switch2Demo.__sourceCode,Prism.languages.html, 'html')"/>
-            </div>
-        </div>
+        <Demo :components="Switch1Demo"/>
+        <Demo :components="Switch2Demo"/>
     </div>
 </template>
 
@@ -37,11 +13,13 @@
     import { ref } from 'vue';
     import 'prismjs';
     import 'prismjs/themes/prism.css';
+    import Demo from './Demo.vue';
 
     const Prism = (window as any).Prism;
 
     export default {
         components: {
+            Demo,
             Button
         },
         setup() {
@@ -57,35 +35,5 @@
 </script>
 
 <style lang="scss" scoped>
-    $border-color: #d9d9d9;
-    .demo {
-        border: 1px solid $border-color;
-        margin: 16px 0 32px;
 
-        > h2 {
-            font-size: 20px;
-            padding: 8px 16px;
-            border-bottom: 1px solid $border-color;
-        }
-
-        &-component {
-            padding: 16px;
-        }
-
-        &-actions {
-            padding: 8px 16px;
-            border-top: 1px dashed $border-color;
-        }
-
-        &-code {
-            padding: 8px 16px;
-            border-top: 1px dashed $border-color;
-
-            > pre {
-                line-height: 1.1;
-                font-family: Consolas, 'Courier New', Courier, monospace;
-                margin: 0;
-            }
-        }
-    }
 </style>
