0dfd889700d8aaa904df7e1bc2db569306fc9485
Suno <dev.suno01@gmail.com>
2020-11-18 17:23:38 +0800

    添加 跳转链接

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,7 +4,7 @@
             <h1>dingdang-ui</h1>
             <h2>一个厉害的 UI 框架</h2>
             <p class="actions">
-                <a href="https://github.com">GitHub</a>
+                <a href="https://github.com/DevSuno/dingdang-ui">GitHub</a>
                 <router-link to="/doc">开始</router-link>
             </p>
         </div>
