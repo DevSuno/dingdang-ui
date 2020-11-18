e91aa0441158150cac5e659ce68aade1afc35763
Suno <dev.suno01@gmail.com>
2020-11-18 14:14:12 +0800

    更新 Tabs 组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,26 +1,23 @@
 <template>
     <div>
         <h1>示例 1</h1>
-        <Tabs v-model:selected="x">
-            <Tab title="导航1">内容1</Tab>
-            <Tab title="导航2">内容2</Tab>
-        </Tabs>
+        <Demo :component="Tab1Demo"/>
     </div>
 </template>
 
 <script lang="ts">
-    import Tab from '../lib/Tab.vue';
-    import Tabs from '../lib/Tabs.vue';
-    import { ref } from 'vue';
+    import Demo from './Demo.vue';
+    import Tab1Demo from './Tab/Tab1.demo.vue';
 
     export default {
+        props: {
+            component: Object
+        },
         components: {
-            Tab, Tabs
+            Demo
         },
         setup() {
-            const x = ref('导航1');
-
-            return { x };
+            return { Tab1Demo };
         }
     };
 </script>
